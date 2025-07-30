61e3e29e87969213642ac9cc80e939a0ec4afe3f
Jasan-Aj <majasan9@gmail.com>
2025-07-30 16:35:12 +0530

    router changed

--- Core/routes.php
+++ Core/routes.php
@@ -1,19 +1,20 @@
 <?php 
-   $router->get('/QSPACE/','Controllers/index.php')->only('guest');
-   $router->get('/QSPACE/rooms','Controllers/Rooms/index.php');
-   $router->get('/QSPACE/login','Controllers/user/login.php')->only('guest');
-   $router->get('/QSPACE/create-room','Controllers/Rooms/create.php')->only('auth');
-   $router->get('/QSPACE/register','Controllers/user/register.php')->only('guest');
-   $router->get('/QSPACE/join-notice/','Controllers/room/notice.php')->only('auth');
-   $router->get('/QSPACE/myrooms','Controllers/room/index.php')->only('auth');
-   $router->get('/QSPACE/chat/','Controllers/room/chat.php')->only('auth');
-
-   $router->post('/QSPACE/store','Controllers/Rooms/store.php');
    
-   $router->post('/QSPACE/store-user','Controllers/user/store.php');
-   $router->post('/QSPACE/session','Controllers/user/sessions.php');
+   $router->get('/git/Qspace/','Controllers/index.php')->only('guest');
+   $router->get('/git/Qspace/rooms','Controllers/Rooms/index.php');
+   $router->get('/git/Qspace/login','Controllers/user/login.php')->only('guest');
+   $router->get('/git/Qspace/create-room','Controllers/Rooms/create.php')->only('auth');
+   $router->get('/git/Qspace/register','Controllers/user/register.php')->only('guest');
+   $router->get('/git/Qspace/join-notice/','Controllers/room/notice.php')->only('auth');
+   $router->get('/git/Qspace/myrooms','Controllers/room/index.php')->only('auth');
+   $router->get('/git/Qspace/chat/','Controllers/room/chat.php')->only('auth');
 
-   $router->delete('/QSPACE/logout','Controllers/user/logout.php')->only('auth');
+   $router->post('/git/Qspace/store','Controllers/Rooms/store.php');
+   
+   $router->post('/git/Qspace/store-user','Controllers/user/store.php');
+   $router->post('/git/Qspace/session','Controllers/user/sessions.php');
 
-   $router->put('/QSPACE/join-notice/store','Controllers/room/store.php');
+   $router->delete('/git/Qspace/logout','Controllers/user/logout.php')->only('auth');
+
+   $router->put('/git/Qspace/join-notice/store','Controllers/room/store.php');
 ?>
